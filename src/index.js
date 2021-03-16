589748d685c51a0ed2267134d80b0204d27d99e7
Cale Haug <caleahaug@gmail.com>
2021-03-15 17:01:31 -0700

    try dat

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+// import { createStore, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
 // import logger from 'redux-logger'
-import { Provider } from 'react-redux'
-import { BrowserRouter, Switch } from 'react-router-dom'
-import { reducer } from './reducers'
+// import { Provider } from 'react-redux'
+// import { BrowserRouter, Switch } from 'react-router-dom'
+// import { reducer } from './reducers'
 
 
 // const store = createStore(reducer, applyMiddleware(thunk, logger))
-const store = createStore(reducer, applyMiddleware(thunk))
+// const store = createStore(reducer, applyMiddleware(thunk))
 
 
-ReactDOM.hydrate(
-    <BrowserRouter>
-      <Switch>
-        <App />
-      </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+ReactDOM.hydrate(<App />, document.getElementById('root'));
