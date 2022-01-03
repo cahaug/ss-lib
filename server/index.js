f09691bab722b9761e1bd07a2593175e656c6b0f
Cale Haug <caleahaug@gmail.com>
2022-01-02 18:18:43 -0700

    finding host in express

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.post('/py0re%C3%84%C3%A4%C3%A4ni', bouncer.block, async (req, res) => {
 })
 
 app.get('/:id', bouncer.block, async (req, res) => {
-  console.log('req.hostame', req.hostname)
+  console.log('req.originalURL', req.originalUrl)
   res.sendFile(path.join(__dirname + '/izvinite.html'));
 })
 
